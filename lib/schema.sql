e8fab3231195761b9314d0a7bc5514993fb06bc1
Getu Gebre <getu.gebre.gg@gmail.com>
2020-10-27 08:38:44 -0500

    done

---
+++ lib/schema.sql
@@ -0,0 +1,34 @@
+
+CREATE TABLE series (
+    title TEXT,
+    author_id INTEGER,
+    subgenre_id INTEGER,
+    id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE subgenres (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+);
+CREATE TABLE books (
+    title TEXT,
+    year INTEGER,
+    series_id INTEGER,
+    id INTEGER PRIMARY KEY
+);
+CREATE TABLE authors (
+    name TEXT,
+    id INTEGER PRIMARY KEY
+);
+CREATE TABLE characters (
+    name TEXT,
+    species TEXT,
+    motto TEXT,
+    author_id INTEGER,
+    id INTEGER PRIMARY KEY
+);
+CREATE TABLE character_books (
+    id INTEGER PRIMARY KEY,
+    book_id INTEGER,
+    character_id INTEGER
+);
